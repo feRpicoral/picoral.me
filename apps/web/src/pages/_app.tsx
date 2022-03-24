3f6aa0e4bf850d8f7a50319a89d9941bc37078e6
Fernando Picoral <fernando.picoral@poatek.com>
2022-03-23 22:01:43 -0300

    chore: added styled-components & pre-commit hooks

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,7 +1,29 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import smoothscroll from 'smoothscroll-polyfill';
+import { createGlobalStyle } from 'styled-components';
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+const GlobalStyle = createGlobalStyle`
+  html, 
+  body {
+    margin: 0;
+    padding: 0;
+    scroll-behavior: smooth;
+  }
+  * {
+    box-sizing: border-box; 
+  } 
+`;
+
+export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        smoothscroll.polyfill();
+    }, []);
+
+    return (
+        <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
 }
-
-export default MyApp;
---
+++ apps/web/src/pages/_document.tsx
@@ -0,0 +1,49 @@
+import Document, {
+    DocumentContext,
+    Head,
+    Html,
+    Main,
+    NextScript
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+    static override async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    // @ts-ignore We don't need to worry with these any's
+                    enhanceApp: App => props =>
+                        sheet.collectStyles(<App {...props} />)
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                )
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+
+    override render() {
+        return (
+            <Html lang="en">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
